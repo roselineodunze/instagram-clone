8bf3d7b27e33a22977591da15859a15dc4a7cc80
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2024-11-24 11:24:24 +0100

    seconf commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Button } from '@chakra-ui/react'
+
+function App() {
+
+  return (
+    <>
+      <Button colorScheme="teal">Click me</Button>
+    </>
+  )
+}
+
+export default App
