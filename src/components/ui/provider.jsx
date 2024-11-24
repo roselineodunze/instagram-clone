f1d90603141a4759baeea8b83b55e3c7883601f9
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2024-11-24 18:03:42 +0100

    check bug

--- src/components/ui/provider.jsx
+++ src/components/ui/provider.jsx
@@ -3,10 +3,11 @@
 import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import { ColorModeProvider } from './color-mode'
 
-export function Provider(props) {
+export function Provider({children}) {
   return (
     <ChakraProvider value={defaultSystem}>
-      <ColorModeProvider {...props} />
+      {/* <ColorModeProvider {...props} /> */}
+      {children}
     </ChakraProvider>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Provider } from './components/ui/provider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider>
+    <Provider>      
       <App />
-    </ChakraProvider>
-
+    </Provider>
   </StrictMode>,
 )
